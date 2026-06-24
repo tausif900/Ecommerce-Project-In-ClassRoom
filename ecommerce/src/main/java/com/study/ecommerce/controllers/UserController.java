57b5c6ce98f8e35f4259380701500b1f75b78052
tausif900 <ansaritausif9268@gmail.com>
2026-06-24 21:57:09 +0530

    added crossorigin

--- ecommerce/src/main/java/com/study/ecommerce/controllers/UserController.java
+++ ecommerce/src/main/java/com/study/ecommerce/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.study.ecommerce.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +16,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
 	@Autowired
