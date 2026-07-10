a72ac4c3353159fab045a7c0ea2241510c80bf66
tausif900 <ansaritausif9268@gmail.com>
2026-07-10 14:55:12 +0530

    adding filteration chain of security

--- ecommerce/src/main/java/com/study/ecommerce/security/SecurityConfig.java
+++ ecommerce/src/main/java/com/study/ecommerce/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.study.ecommerce.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,11 +9,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.study.ecommerce.security.jwt.AuthTokenFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	@Autowired
+	private AuthenticationEntryPoint authenticationEntryPoint;
+
+	@Autowired
+	private AuthTokenFilter authTokenFilter;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
@@ -21,9 +32,15 @@ public class SecurityConfig {
 						.requestMatchers(HttpMethod.POST, "/users").permitAll().anyRequest().authenticated());
 
 		httpSecurity.httpBasic(Customizer.withDefaults());
+		httpSecurity.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+		httpSecurity
+				.exceptionHandling(authentication -> authentication.authenticationEntryPoint(authenticationEntryPoint));
+		
+		
 		return httpSecurity.build();
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
